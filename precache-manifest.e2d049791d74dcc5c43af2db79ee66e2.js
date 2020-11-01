b5f60a690d9b3becb7a7ad85eb21da337923949a
fheyen <frank.heyen@outlook.de>
2020-11-01 23:54:17 +0100

    Updates

---
+++ precache-manifest.e2d049791d74dcc5c43af2db79ee66e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd352bca6fc4815ff2de455becbb16",
+    "url": "/midi-chords/index.html"
+  },
+  {
+    "revision": "5d693d1114a60ed4cc6d",
+    "url": "/midi-chords/static/css/main.8fc47e02.chunk.css"
+  },
+  {
+    "revision": "d34af92c6018a990048a",
+    "url": "/midi-chords/static/js/2.44a9a677.chunk.js"
+  },
+  {
+    "revision": "0cdebb9bd8921e6ffed16c110c74fb19",
+    "url": "/midi-chords/static/js/2.44a9a677.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d693d1114a60ed4cc6d",
+    "url": "/midi-chords/static/js/main.52f93cfa.chunk.js"
+  },
+  {
+    "revision": "c3c0a36a13c0d317c24c",
+    "url": "/midi-chords/static/js/runtime-main.f15e432e.js"
+  }
+]);
